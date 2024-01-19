a7b028f0f56a0e1759b6d86f5efa07b61c0a093c
J222 <baram789456@gmail.com>
2024-01-19 19:47:02 +0100

    [Refactor] Add platform build configs

--- src-tauri/src/event/mod.rs
+++ src-tauri/src/event/mod.rs
@@ -5,6 +5,7 @@ pub fn get_system_info() -> system::dto::SystemInfo {
     system::get_system_info()
 }
 
+#[cfg(target_os = "windows")]
 #[tauri::command]
 pub fn get_windows_system_info() -> String {
     system::get_windows_system_info()
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,9 +5,21 @@ mod event;
 mod system;
 
 
+#[cfg(target_os = "macos")]
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![event::get_system_info, event::get_windows_system_info])
+        .invoke_handler(tauri::generate_handler![event::get_system_info])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
+
+#[cfg(target_os = "windows")]
+fn main() {
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![
+            event::get_system_info,
+            event::get_windows_system_info
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
