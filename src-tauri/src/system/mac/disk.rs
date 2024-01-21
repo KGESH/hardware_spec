ca6c2366dac913f16fa55ec089443de983ceedf2
J222 <baram789456@gmail.com>
2024-01-21 13:59:37 +0100

    [Refactor] Filter only physical disks

--- src-tauri/src/system/mac/disk.rs
+++ src-tauri/src/system/mac/disk.rs
@@ -4,20 +4,28 @@ use crate::system::mac::dto::Disk;
 
 pub fn get_disks_info() -> Vec<Disk> {
     let disks = Disks::new_with_refreshed_list();
-
-    let disks_info = disks.iter().map(|disk| {
+    let disks_info = disks.iter().filter_map(|disk| {
         let name = disk.name().to_str().unwrap().to_string();
         let kind = disk.kind().to_string();
         let file_system = disk.file_system().to_str().unwrap().to_string();
         let total_space = disk.total_space();
         let removable = disk.is_removable();
 
-        Disk {
-            name,
-            kind,
-            file_system,
-            total_space,
-            removable,
+
+        println!("======== Disk ========");
+        println!("{:#?}", disk);
+
+        // Only physical disks
+        if !removable {
+            Some(Disk {
+                name,
+                kind,
+                file_system,
+                total_space,
+                removable,
+            })
+        } else {
+            None
         }
     }).collect::<Vec<Disk>>();
 
