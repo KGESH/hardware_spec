24fbb7997f8c54717798ff3efd3bb4ed9aaca985
J222 <baram789456@gmail.com>
2024-01-21 01:24:13 +0100

    [Refactor] Add panic error log

--- src-tauri/src/system/windows/mod.rs
+++ src-tauri/src/system/windows/mod.rs
@@ -22,16 +22,28 @@ use os::Win32OperatingSystem;
 
 // Todo: remove example code
 pub fn get_windows_system_info() -> Result<dto::WindowsSystem, Box<dyn std::error::Error>> {
+    println!("========get_windows_system_info========");
+
     let com_con = unsafe { COMLibrary::assume_initialized() };
+    println!("========let wmi_con = WMIConnection::new(com_con.into())?;========");
     let wmi_con = WMIConnection::new(com_con.into())?;
 
+
+    println!("========let os = os::get_os_info(&wmi_con)?;========");
     let os = os::get_os_info(&wmi_con)?;
+    println!("========let cpu = cpu::get_cpu_info(&wmi_con)?;========");
     let cpu = cpu::get_cpu_info(&wmi_con)?;
+    println!("========let motherboard = motherboard::get_motherboard_info(&wmi_con)?;========");
     let motherboard = motherboard::get_motherboard_info(&wmi_con)?;
+    println!("========let rams = memory::get_rams_info(&wmi_con)?;========");
     let rams = memory::get_rams_info(&wmi_con)?;
+    println!("========let disks = disk::get_disks_info(&wmi_con)?;========");
     let disks = disk::get_disks_info(&wmi_con)?;
+    println!("========let gpu = gpu::get_gpu_info(&wmi_con)?;========");
     let gpu = gpu::get_gpu_info(&wmi_con)?;
 
+
+    println!("========let windows_system = dto::WindowsSystem========");
     let windows_system = dto::WindowsSystem {
         os,
         cpu,
@@ -40,6 +52,7 @@ pub fn get_windows_system_info() -> Result<dto::WindowsSystem, Box<dyn std::erro
         disks,
         gpu,
     };
+
 
     Ok(windows_system)
 }
