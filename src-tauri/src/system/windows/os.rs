b93a599a6f623ed953d10d07eb2e168c139c4b8c
J222 <baram789456@gmail.com>
2024-01-21 01:47:04 +0100

    [Refactor] Change panic struct member

--- src-tauri/src/system/windows/os.rs
+++ src-tauri/src/system/windows/os.rs
@@ -66,13 +66,13 @@ pub struct Win32OperatingSystem {
     pub suite_mask: Option<u32>,
     pub system_device: Option<String>,
     pub system_directory: Option<String>,
-    // pub system_drive: Option<String>,
-    // pub total_swap_space_size: Option<u64>,
-    // pub total_virtual_memory_size: Option<u64>,
-    // pub total_visible_memory_size: Option<u64>,
-    // pub windows_directory: Option<String>,
-    // pub quantum_length: Option<u8>,
-    // pub quantum_type: Option<u8>,
+    pub system_drive: Option<String>,
+    pub total_swap_space_size: Option<u64>,
+    pub total_virtual_memory_size: Option<u64>,
+    pub total_visible_memory_size: Option<u64>,
+    pub windows_directory: Option<String>,
+    pub quantum_length: Option<u8>,
+    pub quantum_type: Option<u8>,
     // ... add other fields as needed ...
 }
 
